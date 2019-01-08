879519b20054fd530b4f4dcfe748b5632ce0880b
Sunil Mourya <ayruomlinus@gmail.com>
2019-01-08 06:45:03 +0530

    day#2
    * boost lib check
    * unique_ptr
    * shared_ptr

---
+++ CppRevision/BoostTest.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip>
+#include <boost/version.hpp>
+
+int main(int argc, char *argv[])
+{
+    std::cout << "Boost version: " 
+        << BOOST_VERSION / 100000
+        << "."
+        << BOOST_VERSION / 100 % 1000
+        << "."
+        << BOOST_VERSION % 100
+        << std::endl;
+    return 0;
+}
