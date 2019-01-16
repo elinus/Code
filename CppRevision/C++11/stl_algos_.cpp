188dc3a9ada3bc1c0540b4d027538474e40af239
Sunil Mourya <ayruomlinus@gmail.com>
2019-01-16 13:02:40 +0530

    "Cpp11 - new stl algos and initializer list"

---
+++ CppRevision/C++11/stl_algos_.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main (int argc, char *argv[])
+{
+    std::vector<std::string> vec = { "Hi", "Hello", "test", "first", "second", "third", "fourth" };
+
+    bool res1 = std::all_of(vec.begin(), vec.end(), [](const std::string &str){
+            return str.size() > 0 && ::isupper(str[0]);
+            });
+    std::cout << res1 << "\n";
+    
+    bool res2 = std::any_of(vec.begin(), vec.end(), [](const std::string &str){
+            return str.size() > 0 && ::isupper(str[0]);
+            });
+    std::cout << res2 << "\n";
+    return 0;
+}
+
