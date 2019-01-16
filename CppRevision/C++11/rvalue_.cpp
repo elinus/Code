4f7e0679e46135d23813cc7531d320e1f402a086
Sunil Mourya <ayruomlinus@gmail.com>
2019-01-16 11:53:07 +0530

    Rvalue references

---
+++ CppRevision/C++11/rvalue_.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+class Test {
+    private:
+        int counter;
+    public:
+        Test(){
+            counter = 14;
+        }
+        void increment(){
+            counter++;
+        }
+};
+
+Test getTestObj()
+{
+    return Test();
+}
+
+int main (int argc, char *argv[])
+{
+    getTestObj().increment();
+    return 0;
+}
+
