25425511745c328c41afb7859d84084c381c37ca
Sunil Mourya <ayruomlinus@gmail.com>
2019-01-18 22:22:09 +0530

    StopWatch

---
+++ Utils/StopWatch.cpp
@@ -0,0 +1,24 @@
+#include "StopWatch.h"
+        
+StopWatch::StopWatch(): mStart(std::chrono::steady_clock::now())
+{}
+
+StopWatch::StopWatch(const StopWatch &other): mStart(other.mStart)
+{}
+
+StopWatch& StopWatch::operator = (const StopWatch &other)
+{
+    mStart = other.mStart;
+    return *this;
+}
+
+uint64_t StopWatch::ElapsedMs() const
+{
+    return std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - mStart).count();
+}
+
+std::chrono::steady_clock::time_point StopWatch::Restart()
+{
+    mStart = std::chrono::steady_clock::now();
+    return mStart;
+}
---
+++ Utils/StopWatchMain.cpp
@@ -0,0 +1,16 @@
+#include "StopWatch.h"
+#include <iostream>
+#include <thread>
+#include <chrono>
+
+int main (int argc, char *argv[])
+{
+    StopWatch swObj;
+    
+    std::this_thread::sleep_for(std::chrono::milliseconds(5000));
+
+    std::cout << swObj.ElapsedMs() << std::endl;
+
+    return 0;
+}
+
