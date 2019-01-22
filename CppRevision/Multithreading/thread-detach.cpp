b7153034aa85b9370d8bcb4a6ce2ea6701b62c26
Sunil Mourya <ayruomlinus@gmail.com>
2019-01-22 10:36:58 +0530

    thread joining & detaching

---
+++ CppRevision/Multithreading/thread-detach.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <algorithm>
+#include <thread>
+#include <vector>
+#include <functional>
+
+class ThreadTest {
+    public:
+        void operator()() {
+            std::cout << "thread " << std::this_thread::get_id() << " excuting .." << std::endl;
+        }
+};
+
+int main (int argc, char *argv[])
+{
+    std::thread t1((ThreadTest()));
+    if (t1.joinable()) {
+        std::cout << "detaching thread " << t1.get_id() << std::endl;
+        t1.detach();
+    }
+
+    if (t1.joinable()) {
+        std::cout << "detaching thread " << t1.get_id() << std::endl;
+        t1.detach();
+    }
+
+    std::thread t2((ThreadTest())); 
+    if (t2.joinable()) {
+        std::cout << "join thread " << t2.get_id() << std::endl;
+        t2.join();
+    }
+
+
+    if (t2.joinable()) {
+        std::cout << "join thread " << t2.get_id() << std::endl;
+        t2.join();
+    }
+
+    std::cout << "exiting main { id = " << std::this_thread::get_id() << " } thread" << std::endl;
+    return 0;
+}
+
