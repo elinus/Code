7fcfb1fd06f0f7d0507950f504a83e01883c639d
Sunil Mourya <ayruomlinus@gmail.com>
2019-01-22 09:54:27 +0530

    "Thread create"
    * COMPILE COMMAND: g++ –std=c++11 sample.cpp -lpthread
    * - using function pointer
    * - using function objects
    * - using fucntion lambda functions

---
+++ CppRevision/Multithreading/thread-create-func-obj.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <thread>
+
+class Test {
+    public:
+        void operator()() {
+            for (int i = 0; i < 10; i++) {
+                std::cout << "thread function executing.. [ id = " << std::this_thread::get_id() << " ]" << std::endl;
+            }
+        }
+};
+
+int main (int argc, char *argv[])
+{
+    std::thread t1((Test()));
+    for (int i = 0; i < 10; i++) {
+        std::cout << "main function executing.. { id = " << t1.get_id() << " }"<< std::endl;
+    }
+
+    t1.join();
+    std::cout << "Exiting main function!" << std::endl;
+    return 0;
+}
+
