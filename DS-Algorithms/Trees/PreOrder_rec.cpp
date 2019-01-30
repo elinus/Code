feec94ab95fe87fa01639f809dc113d2157016c7
Sunil Mourya <ayruomlinus@gmail.com>
2019-01-30 21:57:03 +0530

    Init DS-Algo, Starting with Trees.
    * [Book] Data Structures and Algorithms Made Easy
    * Tree traversals
    * Recursive: Inorder, Preorder, Postorder
    * Iterative: LevelOrder

---
+++ DS-Algorithms/Trees/PreOrder_rec.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+/* Binary Tree Node */
+struct Node {
+    
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int data) {
+        this->data = data;
+        left = right = nullptr;
+    }
+};
+
+void preorder(Node *root) {
+    if (root != nullptr) {
+        std::cout << root->data << " ";
+        preorder(root->left);
+        preorder(root->right);
+    }
+}
+
+int main (int argc, char *argv[])
+{
+    struct Node *root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->left = new Node(6);
+    root->right->right = new Node(7);
+
+    std::cout << "Preorder Traversal: ";
+    preorder(root);
+    std::cout << "\n";
+    return 0;
+}
+
