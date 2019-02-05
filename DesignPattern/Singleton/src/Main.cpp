c5330e8999f48869097e800a968dc87de8f7c965
Sunil Mourya <ayruomlinus@gmail.com>
2019-02-05 06:34:44 +0530

    Singleton pattern - added multithreading.
    * Issues: object destrcutor is not getting called

--- DesignPattern/Singleton/src/Main.cpp
+++ DesignPattern/Singleton/src/Main.cpp
@@ -1,15 +1,33 @@
 #include "ThreadPoolManager.h"
 #include <iostream>
+#include <thread>
+#include <vector>
 
-int main() {
-
+void AssignAndExecuteTask() {
+  std::cout << "Assigning and excuting task [tid = "
+            << std::this_thread::get_id() << "]" << std::endl;
   ThreadPoolManager *manager1 = ThreadPoolManager::GetInstance();
   manager1->AddJob();
   manager1->ExecuteJob();
+}
 
-  ThreadPoolManager *manager2 = ThreadPoolManager::GetInstance();
-  manager2->AddJob();
-  manager2->ExecuteJob();
+int main() {
+
+  std::vector<std::thread> thVec;
+  int n_threads = 10;
+  for (int i = 0; i < n_threads; ++i) {
+    thVec.emplace_back(AssignAndExecuteTask);
+  }
+
+  for (auto &th : thVec) {
+    if (th.joinable()) {
+      std::cout << "Joining thread [tid = " << th.get_id() << "]" << std::endl;
+      th.join();
+    }
+  }
+
+  std::cout << "Exiting main [tid = " << std::this_thread::get_id() << "]"
+            << std::endl;
 
   return 0;
 }
