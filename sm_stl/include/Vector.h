ad0d24b080f9a9671f39a666c6e1ea1a755e8fa2
Sunil Mourya <ayruomlinus@gmail.com>
2019-02-10 19:16:02 +0530

    Rule of Three

--- sm_stl/include/Vector.h
+++ sm_stl/include/Vector.h
@@ -1,27 +1,41 @@
 #ifndef __VECTOR_H__
 #define __VECTOR_H__
 
-#include <string>
 #include <iostream>
+#include <string>
 
 namespace sm_stl {
 
-    template <typename T>
-    class Vector {
-        public:
-        Vector(size_t size = 10) : size(size), buffer(new T[size])
-        {
-            std::cout << TAG << "Constructor!. size = " << size <<  ", buffer addr. = " << buffer << "\n";
-        }
-        ~Vector() {
-            std::cout << TAG << "Destructor!. deleting buffer addr. = " << buffer << "\n";
-            delete [] buffer;
-        }
-        private:
-            std::string TAG = "[Vector] ";
-            std::size_t size;
-            T *buffer;
-    };
-}
+template <typename T> class Vector {
+public:
+  Vector(size_t size = 10) : size(size), buffer(new T[size]) {
+    std::cout << TAG << "Constructor!. size = " << size
+              << ", buffer addr. = " << buffer << "\n";
+  }
+
+  ~Vector() {
+    std::cout << TAG << "Destructor!. deleting buffer addr. = " << buffer
+              << "\n";
+    delete[] buffer;
+  }
+
+  Vector(const Vector &other) : size(other.size), buffer(new T[size]) {
+    std::cout << TAG << "Copy Constructor!. size = " << size
+              << ", buffer addr. = " << buffer << "\n";
+    std::copy(other.buffer, other.buffer + other.size, buffer);
+  }
+
+  Vector &operator=(const Vector &other) {
+    std::cout << TAG << "Assign operator!. size = " << size
+              << ", buffer addr. = " << buffer << "\n";
+    *this;
+  }
+
+private:
+  std::string TAG = "[Vector] ";
+  std::size_t size;
+  T *buffer;
+};
+} // namespace sm_stl
 
 #endif /* __VECTOR_H__ */
--- sm_stl/src/Test.cpp
+++ sm_stl/src/Test.cpp
@@ -1,11 +1,11 @@
-#include <iostream>
 #include "Vector.h"
+#include <iostream>
 
 int main() {
 
-    sm_stl::Vector<int> x;
-    sm_stl::Vector<int> y(x);
-    sm_stl::Vector<int> z;
-    z = x;
-    return 0;
+  sm_stl::Vector<int> x;
+  sm_stl::Vector<int> y(x);
+  sm_stl::Vector<int> z;
+  z = x;
+  return 0;
 }
