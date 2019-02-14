a673f62bbb4c853139417b7f0665b5f604c5d862
Sunil Mourya <ayruomlinus@gmail.com>
2019-02-15 00:05:48 +0530

    Refactoring-15.02.2019.00.05

--- cmake_libs/Helloworld/src/Helloworld.cpp
+++
@@ -1,7 +0,0 @@
-#include <Helloworld/Helloworld.h>
-
-void Helloworld::sayHello(int n) {
-  for (int i = 0; i < n; i++) {
-    std::cout << "Hello world !! { " << i << " }" << std::endl;
-  }
-}
--- competitive-programming/LeetCode/move-zeroes.cpp
+++ competitive-programming/LeetCode/move-zeroes.cpp
@@ -4,41 +4,40 @@
 using namespace std;
 
 class Solution {
-    public:
-        void moveZeroes(vector<int>& nums) {
-            int count = 0;
-            for (int i = 0; i < nums.size(); ++i) {
-                if (nums[i]) {
-                    count++;
-                }
-            }
-            int j = 0;
-            for (int i = 0; i < nums.size() && j < count; i++) {
-                if (nums[i]) {
-                    nums[j++] = nums[i];
-                }
-            }
-            int zeros = nums.size() - count;
-            j = nums.size() - 1;
-            for (int i = zeros; i > 0; --i) {
-                nums[j--] = 0;
-            }
-        }
+public:
+  void moveZeroes(vector<int> &nums) {
+    int count = 0;
+    for (int i = 0; i < nums.size(); ++i) {
+      if (nums[i]) {
+        count++;
+      }
+    }
+    int j = 0;
+    for (int i = 0; i < nums.size() && j < count; i++) {
+      if (nums[i]) {
+        nums[j++] = nums[i];
+      }
+    }
+    int zeros = nums.size() - count;
+    j = nums.size() - 1;
+    for (int i = zeros; i > 0; --i) {
+      nums[j--] = 0;
+    }
+  }
 };
 
 int main() {
 
-    vector<int> vec = {0, 1, 2, 3, 0, 4, 5, 0, 6, 7, 0, 8, 9, 0};
-    for (auto it = vec.begin(); it != vec.end(); ++it) {
-        cout << *it << " ";
-    }
-    cout << endl;
-    Solution obj;
-    obj.moveZeroes(vec);
-    for (auto it = vec.begin(); it != vec.end(); ++it) {
-        cout << *it << " ";
-    }
-    cout << endl;
-    return 0;
-
+  vector<int> vec = {0, 1, 2, 3, 0, 4, 5, 0, 6, 7, 0, 8, 9, 0};
+  for (auto it = vec.begin(); it != vec.end(); ++it) {
+    cout << *it << " ";
+  }
+  cout << endl;
+  Solution obj;
+  obj.moveZeroes(vec);
+  for (auto it = vec.begin(); it != vec.end(); ++it) {
+    cout << *it << " ";
+  }
+  cout << endl;
+  return 0;
 }
