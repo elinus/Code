bc975caa3b067cf6356e3241560414a8ce086bc4
Sunil Mourya <ayruomlinus@gmail.com>
2019-03-06 22:41:28 +0530

    clang-format - 06.03.2019.22.41

--- CppRevision/MemberFunctionPointer.cpp
+++ CppRevision/MemberFunctionPointer.cpp
@@ -3,69 +3,54 @@
 
 using namespace std;
 
-//Normal function pointer
-void printString(string s)
-{
-    cout << s << endl;
-}
+// Normal function pointer
+void printString(string s) { cout << s << endl; }
 
-//Member function pointer.
-class funcPointer
-{
-  public:
-    void printString(string s)
-    {
-        cout << s << endl;
-    }
+// Member function pointer.
+class funcPointer {
+public:
+  void printString(string s) { cout << s << endl; }
 };
 
-//Polymorphism use.
-class Base
-{
-  public:
-    virtual void sayHello()
-    {
-        cout << "Hello from Base class. \n";
-    }
+// Polymorphism use.
+class Base {
+public:
+  virtual void sayHello() { cout << "Hello from Base class. \n"; }
 };
 
-class Derived : public Base
-{
-  public:
-    void sayHello()
-    {
-        cout << "Hello from Derived class. \n";
-    }
+class Derived : public Base {
+public:
+  void sayHello() { cout << "Hello from Derived class. \n"; }
 };
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
 
-    //Standard function pointer example
-    string ss = "Hello World.";
+  // Standard function pointer example
+  string ss = "Hello World.";
 
-    void (*funcPtr)(string) = &printString;
+  void (*funcPtr)(string) = &printString;
 
-    (*funcPtr)(ss);
+  (*funcPtr)(ss);
 
-    //Member function pointer example.
-    string str = "Hello from member function pointer.!!";
-    void (funcPointer::*memFuncPtr)(string) = &funcPointer::printString;
+  // Member function pointer example.
+  string str = "Hello from member function pointer.!!";
+  void (funcPointer::*memFuncPtr)(string) = &funcPointer::printString;
 
-    funcPointer fP;
+  funcPointer fP;
 
-    (fP.*memFuncPtr)(str);
+  (fP.*memFuncPtr)(str);
 
-    //Virtual member function example.
-    void (Base::*baseFuncPtr)() = &Base::sayHello;
-    void (Derived::*derivedFuncPtr)() = &Derived::sayHello;
+  // Virtual member function example.
+  void (Base::*baseFuncPtr)() = &Base::sayHello;
+  void (Derived::*derivedFuncPtr)() = &Derived::sayHello;
 
-    Base bP;
-    Derived dP;
+  Base bP;
+  Derived dP;
 
-    (bP.*baseFuncPtr)();    //call Base::sayHello
-    (dP.*derivedFuncPtr)(); //call Derived::sayHello
-    (dP.*baseFuncPtr)();    //call Derived::sayHello using base pointer and derived object.
+  (bP.*baseFuncPtr)();    // call Base::sayHello
+  (dP.*derivedFuncPtr)(); // call Derived::sayHello
+  (dP.*baseFuncPtr)(); // call Derived::sayHello using base pointer and derived
+                       // object.
 
-    return 0;
+  return 0;
 }
