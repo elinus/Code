dd2685bcd3eb217492d26f0504f0f127aa387dc8
Sunil Mourya <sunilmourya.ie@gmail.com>
2019-03-28 22:17:46 +0530

    Comparable & Comparator

---
+++ javanotes/src/InterviewPreparation2x19/RatingCompare.java
@@ -0,0 +1,23 @@
+/**
+ * Created on 28-Mar-19.
+ *
+ * @author Sunil Mourya
+ * @description
+ * @link
+ * @Input:
+ * @Output:
+ * @Complexity:
+ */
+
+package InterviewPreparation2x19;
+
+import java.util.Comparator;
+
+public class RatingCompare implements Comparator<Movie> {
+    @Override
+    public int compare(Movie o1, Movie o2) {
+        if (o1.getRating() < o2.getRating()) return -1;
+        else if (o1.getRating() > o2.getRating()) return 1;
+        else return 0;
+    }
+}
