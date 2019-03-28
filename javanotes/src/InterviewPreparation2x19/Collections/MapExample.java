0a76645130ab099e0b0503aa1b13d49a9cdd3b61
Sunil Mourya <sunilmourya.ie@gmail.com>
2019-03-28 22:47:28 +0530

    TreeMap

--- javanotes/src/InterviewPreparation2x19/Collections/MapExample.java
+++ javanotes/src/InterviewPreparation2x19/Collections/MapExample.java
@@ -14,17 +14,29 @@ package InterviewPreparation2x19.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class MapExample {
     public static void main(String[] args) {
-        Map<String, String> map = new HashMap<String, String>();
-        map.put("one", "first");
-        map.put("two", "second");
-        map.put("three", "third");
-        Iterator iterator = map.keySet().iterator();
+        System.out.println("---------- H A S H M A P----------");
+        Map<String, String> hashMap = new HashMap<String, String>();
+        hashMap.put("one", "first");
+        hashMap.put("two", "second");
+        hashMap.put("three", "third");
+        Iterator iterator = hashMap.keySet().iterator();
         while (iterator.hasNext()) {
             Object key = iterator.next();
-            Object value = map.get(key);
+            Object value = hashMap.get(key);
+            System.out.println("key = " + key + ", value = " + value);
+        }
+
+        System.out.println("---------- T R E E M A P ----------");
+        Map<String, String> treeMap = new TreeMap<String, String>();
+        treeMap.putAll(hashMap);
+        iterator = treeMap.keySet().iterator();
+        while (iterator.hasNext()) {
+            Object key = iterator.next();
+            Object value = treeMap.get(key);
             System.out.println("key = " + key + ", value = " + value);
         }
     }
