04f47745acb632310c26d9c84d3784b322565dc8
Sunil Mourya <ayruomlinus@gmail.com>
2019-04-04 18:51:17 +0530

    Acrhiving old multithreading code

--- CppRevision/Archived-04042019/thread-create-func-ptr.cpp
+++ CppRevision/Archived-04042019/thread-create-func-ptr.cpp
@@ -2,7 +2,7 @@
 #include <thread>
 
 void foo() {
-  for (int i = 0; i < 1000; i++) {
+  for (int i = 0; i < 10; i++) {
     std::cout << "thread function executing.. [ id = "
               << std::this_thread::get_id() << " ]" << std::endl;
   }
@@ -10,7 +10,7 @@ void foo() {
 
 int main(int argc, char *argv[]) {
   std::thread t1(foo);
-  for (int i = 0; i < 1000; i++) {
+  for (int i = 0; i < 10; i++) {
     std::cout << "main function executing.. { id = " << t1.get_id() << " }"
               << std::endl;
   }
