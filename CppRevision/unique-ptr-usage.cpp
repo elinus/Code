9d522a7a2fc91d7c766ddeae93aa32f5cc9cdc66
Sunil Mourya <ayruomlinus@gmail.com>
2019-07-02 20:28:55 +0530

    Day#2, More experiment with unique ptr
    * passing unique_ptr as func parameter
    * return unique_ptr from func
    * using assert instead of nullptr check

--- CppRevision/unique-ptr-usage.cpp
+++ CppRevision/unique-ptr-usage.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
 #include <memory>
+#include <cassert>
+
+void foo(std::unique_ptr<int> uptr) {
+    std::cout << __func__ << ": " << *uptr << std::endl;
+}
+
+std::unique_ptr<int> bar() {
+    std::unique_ptr<int> buptr = std::make_unique<int>(14); 
+    return buptr;
+}
 
 int main (int argc, char const *argv[])
 {
     std::unique_ptr<int> p1(new int(14));
     std::cout << *p1 << std::endl; 
+    assert(p1); // p1 != nullptr
+    foo(std::move(p1));
+    assert(!p1); // p1 == nullptr
+   
+    auto p2 = bar();
+    std::cout << *p2 << std::endl; 
+
     return 0;
 }
 
