6be6470499b8ab0c70510c5ddd524177a7e5f677
Sunil Mourya <ayruomlinus@gmail.com>
2019-07-04 22:24:38 +0530

    Day#4, Thread creation using lambda function

--- CppRevision/C++11-Multithreading/ThreadCreate.cpp
+++ CppRevision/C++11-Multithreading/ThreadCreate.cpp
@@ -7,14 +7,28 @@ void foo() {
     }
 }
 
+void bar() {
+    for (int i = 0; i < 5; ++i) {
+        std::cout << __func__ << ": " << i << std::endl;
+    }
+}
+
 int main(int argc, char const *argv[])
 {
     /* Thread creation using function pointer */
     std::thread t1(foo);
-    for (int i = 0; i < 5; ++i) {
-        std::cout << __func__ << ": " << i << std::endl;
-    }
+    bar();
     t1.join();
+
+    /* Thread creation using lambda function */
+    std::thread t2([]{
+            for (int i = 0; i < 5; i++) {
+                std::cout << "Lambda func: " << __func__ << ":=> " << i << std::endl;
+            }
+            });
+    bar();
+    t2.join();
+
     std::cout << "Exiting main thread!" << std::endl;
     return 0;
 }
