8c7ebd37fee53c9767bc5cd1d93c77bf9359067a
Sunil Mourya <ayruomlinus@gmail.com>
2019-07-06 22:39:38 +0530

    Day#6, Thread as a member variable in class, MAIN & USAGE

--- CppRevision/C++11-Multithreading/ThreadWrapper.cpp
+++ CppRevision/C++11-Multithreading/ThreadWrapper.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <thread>
 #include <functional>
+#include <vector>
 
 class ThreadWrapper {
     private:
         std::thread threadHandler;
     public:
-        ThreadWrapper(std::function<void> func): 
+        ThreadWrapper(std::function<void()> func): 
             threadHandler(func) {}
         ~ThreadWrapper() { 
             if (threadHandler.joinable()) {
@@ -15,12 +16,12 @@ class ThreadWrapper {
         }
 
         ThreadWrapper(const ThreadWrapper &) = delete;
-        ThreadWrapper& operator =(const ThreadWrapper &) = delete;
+        ThreadWrapper& operator=(ThreadWrapper &) = delete;
         ThreadWrapper(ThreadWrapper && other): 
             threadHandler(std::move(other.threadHandler)) {
                 std::cout << "Move Constructor is called!" << std::endl;
         }
-        ThreadWrapper& operator =(const ThreadWrapper && other) {
+        ThreadWrapper& operator =(ThreadWrapper && other) {
             std::cout << "Move Assignment is called!" << std::endl;
             if (threadHandler.joinable()) {
                 threadHandler.join();
@@ -33,6 +34,25 @@ class ThreadWrapper {
 
 int main(int argc, char const *argv[])
 {
+    std::function<void()> func = []() {
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+        std::cout << __func__ << " :: " << std::this_thread::get_id() << std::endl;
+    };
+
+    { // Block
+        ThreadWrapper wrapper(func);
+    }
+
+    std::vector<ThreadWrapper> threadList;
+    ThreadWrapper t1(func);
+    ThreadWrapper t2(func);
+    
+    threadList.push_back(std::move(t1));
+    threadList.push_back(std::move(t2));
+
+    ThreadWrapper t3(func);
+
+    threadList[1] = std::move(t3);
     
     return 0;
 }
