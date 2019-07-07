7d82b41b087f6a390c601d6948a6c8221482c3ff
Sunil Mourya <ayruomlinus@gmail.com>
2019-07-07 14:03:33 +0530

    Day#7, Event handling using condition_variable

--- CppRevision/C++11-Multithreading/EventHandling.cpp
+++ CppRevision/C++11-Multithreading/EventHandling.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
 #include <thread>
 #include <mutex>
+#include <functional>
+#include <condition_variable>
+
+using namespace std::placeholders;
 
 class Application {
     private:
         std::mutex mMutex;
         bool mDataLoaded;
+        std::condition_variable mCondVar;
     public:
         Application(): mDataLoaded(false) {}
         void loadData() {
@@ -13,17 +18,23 @@ class Application {
             std::cout << "Loading data from xml." << std::endl;
             std::lock_guard<std::mutex> lg(mMutex);
             mDataLoaded = true;
+            mCondVar.notify_one();
+        }
+        bool isDataLoaded() {
+            return mDataLoaded;
         }
         void mainTask() {
             std::cout << "Do some handshaking!" << std::endl;
-            mMutex.lock();
-            while (mDataLoaded != true) {
-                mMutex.unlock();
-                std::cout << "Data not loaded. Sleeing..!" << std::endl;
-                std::this_thread::sleep_for(std::chrono::milliseconds(500));
-                mMutex.lock();
-            }
-            mMutex.unlock();
+            std::unique_lock<std::mutex> ul(mMutex);
+            //mMutex.lock();
+            //while (mDataLoaded != true) {
+                //mMutex.unlock();
+                std::cout << "Data not loaded. Sleeping..!" << std::endl;
+                //std::this_thread::sleep_for(std::chrono::milliseconds(500));
+                //mMutex.lock();
+            //}
+            //mMutex.unlock();
+            mCondVar.wait(ul, std::bind(&Application::isDataLoaded, this));
             std::cout << "Do processing on loaded data!" << std::endl;
         }
 };
