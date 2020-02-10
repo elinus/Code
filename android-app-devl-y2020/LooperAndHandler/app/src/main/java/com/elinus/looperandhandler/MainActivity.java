e9e8be5381d7cdcaf91620a35a13d139fffe4e79
Sunil Mourya <ayruomlinus@gmail.com>
2020-02-10 20:50:54 +0530

    Simple worker thread class

--- android-app-devl-y2020/LooperAndHandler/app/src/main/java/com/elinus/looperandhandler/MainActivity.java
+++ android-app-devl-y2020/LooperAndHandler/app/src/main/java/com/elinus/looperandhandler/MainActivity.java
@@ -1,14 +1,61 @@
 package com.elinus.looperandhandler;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SimpleWorker worker = null;
+    private TextView tvMessage = null;
+    private Handler handler = new Handler(Looper.getMainLooper()) {
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            super.handleMessage(msg);
+            tvMessage.setText((String) msg.obj);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvMessage = findViewById(R.id.message);
+
+        worker = new SimpleWorker();
+        worker.execute(() -> {
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            Message msg = Message.obtain();
+            msg.obj = "Task-1 Completed!";
+            handler.sendMessage(msg);
+        }).execute(() -> {
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            Message msg = Message.obtain();
+            msg.obj = "Task-2 Completed!!";
+            handler.sendMessage(msg);
+        }).execute(() -> {
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            Message msg = Message.obtain();
+            msg.obj = "Task-3 Completed!!!";
+            handler.sendMessage(msg);
+        });
     }
 }
