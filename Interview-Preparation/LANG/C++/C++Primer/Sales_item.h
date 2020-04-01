d172bdedc464cef56eb4943f3ac72720a965be6c
Sunil Mourya <sunilmourya.cse.nitp@gmail.com>
2020-04-01 21:58:56 +0530

    Code - 01042020_2158

--- Interview-Preparation/LANG/C++/C++Primer/Sales_item.h
+++ Interview-Preparation/LANG/C++/C++Primer/Sales_item.h
@@ -14,7 +14,7 @@ class Sales_item {
         Sales_item(const std::string & book):
             bookNo(book), units_sold(0), revenue(0.0) {}
         Sales_item(std::istream & is) {
-            is << *this;
+            is >> *this;
         }
         std::string isbn() const {
             return bookNo;
@@ -26,5 +26,47 @@ class Sales_item {
         unsigned units_sold;
         double revenue;
 };
+
+double Sales_item::avg_price() const {
+    if (units_sold)
+        return revenue / units_sold;
+    return 0.0;
+}
+
+std::istream & operator>>(std::istream & in, Sales_item & si) {
+    double price;
+    in >> si.bookNo >> si.units_sold >> price;
+    if (in) 
+        si.revenue = si.units_sold * price;
+    else
+        si = Sales_item();
+    return in;
+}
+
+std::ostream & operator<<(std::ostream & out, const Sales_item & si) {
+    out << si.isbn() << " " << si.units_sold << " " 
+        << si.revenue << " " << si.avg_price();
+    return out;
+}
+
+Sales_item & Sales_item::operator+=(const Sales_item & rhs) {
+    units_sold += rhs.units_sold;
+    revenue += rhs.revenue;
+    return *this;
+}
+
+Sales_item operator+(const Sales_item & lhs, const Sales_item & rhs) {
+    Sales_item ret(lhs);
+    ret += rhs;
+    return ret;
+}
+
+bool operator==(const Sales_item & lhs, const Sales_item & rhs) {
+    return lhs.units_sold == rhs.units_sold && lhs.revenue == rhs.revenue && lhs.isbn() == rhs.isbn();
+}
+
+bool operator!=(const Sales_item & lhs, const Sales_item & rhs) {
+    return !(lhs == rhs);
+}
 
 #endif /* __SALES_ITEM_H__ */
