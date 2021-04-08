8985991790d210e3abdf8ae18373e2875bcb2514
__elinus__ <ayruomlinus@gmail.com>
2021-04-08 23:40:01 +0530

    [Day#0] Bit Manipulation get & set

--- BitManipulation/BitManipulation.cpp
+++ BitManipulation/BitManipulation.cpp
@@ -1,9 +1,23 @@
+#include <bitset>
 #include <iostream>
-#include <iomanip>
+
 using namespace std;
 
 int main(int argc, char const *argv[]) {
-    unsigned int a = 6;
-    cout << setbase(2) << a << endl; 
-    return 0;
+  int s = 14;
+  cout << "s = " << bitset<8 * sizeof(int)>(s) << endl;
+
+  int a = 20;
+  cout << "a = " << bitset<8 * sizeof(int)>(a) << endl;
+
+  // get
+  bool out = (a & (1 << 14)) != 0;
+  cout << "a(14) = " << out << endl;
+  out = (a & (1 << 4)) != 0;
+  cout << "a(4) = " << out << endl;
+
+  // set
+  a |= (1 << 14);
+  cout << "a(set=14) = " << bitset<8 * sizeof(int)>(a)  << "\nNew(a) = " << a << endl;
+  return 0;
 }
