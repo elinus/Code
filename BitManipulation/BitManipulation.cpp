83aa0cfd78da4b40206f33d55d321be52ef4bddf
elinus <sunilmourya.nitp.cse@gmail.com>
2021-04-08 23:52:54 +0530

    [Day#0] Clear bit

--- BitManipulation/BitManipulation.cpp
+++ BitManipulation/BitManipulation.cpp
@@ -19,5 +19,11 @@ int main(int argc, char const *argv[]) {
   // set
   a |= (1 << 14);
   cout << "a(set=14) = " << bitset<8 * sizeof(int)>(a)  << "\nNew(a) = " << a << endl;
+
+  // clear bit
+  int mask = ~(1 << 14);
+  a = a & mask;
+  cout << "clear( a = 14) = " << a << endl;
+
   return 0;
 }
