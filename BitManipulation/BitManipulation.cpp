77e5a0a31d40f3cb79fc36068236013004364400
Sunil Mourya <sunilmourya.nitp.cse@gmail.com>
2021-04-08 23:13:25 +0530

    Perseverance begin!

---
+++ BitManipulation/BitManipulation.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+    unsigned int a = 6;
+    cout << setbase(2) << a << endl; 
+    return 0;
+}
---
+++ ij_dev/src/com/coco/MD5.java
@@ -0,0 +1,28 @@
+package com.coco;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class MD5 {
+    public static String getMD5(String input) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] message_digest = md.digest(input.getBytes());
+            BigInteger num = new BigInteger(1, message_digest);
+            String hash_text = num.toString(16);
+            while (hash_text.length() < 32) {
+                hash_text = "0" + hash_text;
+            }
+            return hash_text;
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    public static void main(String[] args) {
+        String url = "https://www.educative.io/courses/grokking-the-system-design-interview/m2yDVZnQ8lG";
+        System.out.println("url = " + url + "\nhash = " + MD5.getMD5(url));
+    }
+}
