8fc6585c3a6b160c1cea753b4a6f971cfcea4649
elinus <sunilmourya.cse.nitp@gmail.com>
2021-05-08 22:40:41 +0530

    [Day#115] InterviewBit - 1493/500

--- InterviewBit/L6-Tree_Data_Structure/BinarySearchTreeImpl.cpp
+++ InterviewBit/L6-Tree_Data_Structure/BinarySearchTreeImpl.cpp
@@ -21,12 +21,12 @@ public:
     else
       return false;
   }
-  
+
   void insert(int value) {
     TreeNode *node = new TreeNode(value);
     TreeNode *parent = nullptr;
     if (isEmpty()) {
-      root = parent;
+      root = node;
     } else {
       TreeNode *curr = root;
       while (curr != nullptr) {
@@ -42,9 +42,41 @@ public:
         parent->right = node;
     }
   }
+
+  TreeNode *insertNode(TreeNode *curr, int data) {
+    if (curr == nullptr)
+      return new TreeNode(data);
+    if (curr->val < data)
+      curr->right = insertNode(curr->right, data);
+    else if (curr->val > data)
+      curr->left = insertNode(curr->left, data);
+
+    return curr;
+  }
+
+  void inOrderUtil(TreeNode *curr) {
+    if (curr == nullptr)
+      return;
+    inOrderUtil(curr->left);
+    cout << curr->val << " ";
+    inOrderUtil(curr->right);
+  }
+
+  void inOrder() {
+    inOrderUtil(root);
+    cout << endl;
+  }
 };
 
 int main(int argc, char const *argv[]) {
-  /* code */
+  BST bst;
+  bst.insert(52);
+  bst.insert(25);
+  bst.insert(50);
+  bst.insert(15);
+  bst.insert(40);
+  bst.insert(45);
+  bst.insert(20);
+  bst.inOrder();
   return 0;
 }
