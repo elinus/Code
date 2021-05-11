f50ed379407f1ae2b5042fa320b2a1635bfbbd2d
elinus <sunilmourya.cse.nitp@gmail.com>
2021-05-11 08:09:00 +0530

    [Day#111] Singleton & Factory design patterns

--- DesignPattern/Creational_Design_Patterns/Singleton/SingletonDatabase.cpp
+++ DesignPattern/Creational_Design_Patterns/Singleton/SingletonDatabase.cpp
@@ -2,8 +2,13 @@
 #include <iostream>
 #include <map>
 #include <string>
+#include <vector>
 
-class SingletonDatabase {
+struct Database {
+  virtual int32_t get_population(const std::string &name) = 0;
+};
+
+class SingletonDatabase : public Database {
 private:
   std::map<std::string, int32_t> m_country;
   SingletonDatabase() {
@@ -22,11 +27,44 @@ public:
     static SingletonDatabase db;
     return db;
   }
-  int32_t get_population(const std::string &name) { return m_country[name]; }
+  int32_t get_population(const std::string &name) override {
+    return m_country[name];
+  }
 };
+
+class DummyDatabase : public Database {
+private:
+  std::map<std::string, int32_t> m_country;
+
+public:
+  DummyDatabase() : m_country{{"alpha", 1}, {"beta", 2}, {"gamma", 3}} {}
+  int32_t get_population(const std::string &name) override {
+    return m_country[name];
+  }
+};
+
+/* Testing class ----------------------------------------------------------*/
+class ConfigurableRecordFinder {
+private:
+  Database &m_db;
+
+public:
+  ConfigurableRecordFinder(Database &db) : m_db{db} {}
+  int32_t total_population(const std::vector<std::string> &countries) {
+    int32_t result = 0;
+    for (auto &country : countries) {
+      result += m_db.get_population(country);
+    }
+    return result;
+  }
+};
+/*-------------------------------------------------------------------------*/
 
 int main(int argc, char const *argv[]) {
   std::cout << "Japan = " << SingletonDatabase::get().get_population("Japan")
             << std::endl;
+  DummyDatabase db;
+  ConfigurableRecordFinder rf(db);
+  std::cout << rf.total_population({"alpha", "beta", "gamma"}) << std::endl;
   return 0;
 }
