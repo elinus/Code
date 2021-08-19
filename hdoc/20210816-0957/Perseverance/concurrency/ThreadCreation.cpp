8f89e429f351d27ce28d4188416699274df066a6
Sunil Mourya <sunilmourya.nitp.cse@gmail.com>
2021-08-19 21:58:14 +0530

    [Day-1//20210819/2158] Thread creation in c++ @Starting my hdoc journey with "thispointer.com" multithreading tutorials

---
+++ hdoc/20210816-0957/Perseverance/concurrency/ThreadCreation.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <functional>
+
+void foo() {
+	std::cout << std::this_thread::get_id() << " ::inside foo" << std::endl;
+	std::this_thread::sleep_for(std::chrono::milliseconds(500));
+	std::cout << std::this_thread::get_id() << " thread exiting.." << std::endl;
+}
+
+struct Buzz {
+	void operator()() {
+		std::cout << std::this_thread::get_id() << " ::inside foo" << std::endl;
+		std::this_thread::sleep_for(std::chrono::milliseconds(700));
+		std::cout << std::this_thread::get_id() << " thread exiting.." << std::endl;
+	}
+};
+
+void bar() {
+	std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+	std::cout << "bar, thread-id = " << std::this_thread::get_id() << std::endl;
+}
+
+int main() {
+	std::cout << "main-thread :: " << std::this_thread::get_id() << std::endl;
+	
+	std::function<void()> lambda_foo = []() {
+		std::cout << std::this_thread::get_id() << " ::inside foo" << std::endl;
+		std::this_thread::sleep_for(std::chrono::milliseconds(1100));
+		std::cout << std::this_thread::get_id() << " thread exiting.." << std::endl;
+
+	};
+
+	std::thread th_foo(foo);
+	std::thread th_func_obj{ Buzz() };
+	std::thread th_lambda(lambda_foo);
+	
+	th_foo.join();
+	th_func_obj.join();
+	th_lambda.join();
+
+	std::thread th_bar(bar);
+	if (th_bar.joinable()) th_bar.detach();
+
+	// waiting for detached thread print
+	std::this_thread::sleep_for(std::chrono::milliseconds(5000));
+
+	std::cout << "exiting main-thread :: " << std::this_thread::get_id() << std::endl;
+ 	return 0;
+}
