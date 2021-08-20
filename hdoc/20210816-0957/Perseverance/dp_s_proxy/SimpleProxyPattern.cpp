0a2bae11a520bcf6d5fb1fd26d5bea9335dfa000
elinus <sunilmourya.cse.nitp@gmail.com>
2021-08-20 23:18:55 +0530

    [Day-2//20210820/2319] Proxy design pattern TODO:// Need to add STL usage code

---
+++ hdoc/20210816-0957/Perseverance/dp_s_proxy/SimpleProxyPattern.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+class Subject {
+public:
+	virtual void request() = 0;
+};
+
+class RealSubject : public Subject {
+public:
+	void request() override {
+		std::cout << "RealSubject: Handling request!" << std::endl;
+	}
+};
+
+class Proxy : public Subject {
+private:
+	RealSubject* real_subject_;
+	bool check_access() const {
+		std::cout << "Proxy: checking access prior firing a real request.\n";
+		return true;
+	}
+	void log_access() const {
+		std::cout << "Proxy: logging the time of access.\n";
+	}
+public:
+	Proxy(RealSubject* real_subject) : real_subject_{new RealSubject(*real_subject)} 
+	{}
+	~Proxy() {
+		delete real_subject_;
+	}
+	void request() override {
+		if (this->check_access()) {
+			this->real_subject_->request();
+			this->log_access();
+		}
+	}
+};
+
+void ClientCode(Subject& subject) {
+	subject.request();
+}
+
+int main() {
+	std::cout << "Client: Executing the client code with a real subject:\n";
+	RealSubject* real_subject = new RealSubject;
+	ClientCode(*real_subject);
+	std::cout << "\n";
+	std::cout << "Client: Executing the same client code with a proxy:\n";
+	Proxy* proxy = new Proxy(real_subject);
+	ClientCode(*proxy);
+	delete real_subject;
+	delete proxy;
+	return 0;
+}
